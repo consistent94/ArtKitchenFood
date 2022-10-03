3f6dab84484ac3975004adf51e2a9d4b18ae18d7
Sebastian W <sebwilczega@gmail.com>
2022-10-03 20:53:28 +0100

    server

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 import { AboutUs, FindUs, Footer, Gallery, Header , SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Gallery />
-    <FindUs />
-    <Footer />
-  </div>
+const App = () => {
+  const [data, setData] = useState(null)
+
+  React.useEffect(() => {
+    fetch("/")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
 );
+  }
 
 export default App;
